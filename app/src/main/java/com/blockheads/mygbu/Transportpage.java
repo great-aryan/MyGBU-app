1ca2acf96a4c9197faa126e4e3237217f86771ee
OSHI <raghavoshi31@gmail.com>
2021-12-19 10:40:55 +0530

    alignment

--- app/src/main/java/com/blockheads/mygbu/Transportpage.java
+++ app/src/main/java/com/blockheads/mygbu/Transportpage.java
@@ -2,18 +2,29 @@ package com.blockheads.mygbu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Transportpage extends AppCompatActivity {
+    Button buttonCall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transportpage);
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().hide();
-        }
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+        buttonCall = findViewById(R.id.btncall);
+
+        buttonCall.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + "8178788527"));// Initiates the Intent
+                startActivity(intent);
+
+            }
+        });
     }
 }
