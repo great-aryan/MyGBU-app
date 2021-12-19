93a9a6374999032a8acd1235b6633081f563c7a9
OSHI <raghavoshi31@gmail.com>
2021-12-19 13:31:10 +0530

    FEE BUTTON

--- app/src/main/java/com/blockheads/mygbu/Admissionpage.java
+++ app/src/main/java/com/blockheads/mygbu/Admissionpage.java
@@ -2,20 +2,34 @@ package com.blockheads.mygbu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class Admissionpage extends AppCompatActivity {
+    ImageButton feebutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admissionpage);
 
+        feebutton= findViewById(R.id.feebutton);
+
         setContentView(R.layout.activity_admissionpage);
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+        feebutton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent i = new Intent(Admissionpage.this, FeeActivity.class);
+            startActivity(i);
+        }
+        });
     }
 }
