56880afed193df76203f95c8b0b04189beb9ee9e
Great Aryan <aryans2808@gmail.com>
2021-12-19 20:04:33 +0530

    helpdesk

--- app/src/main/java/com/blockheads/mygbu/MapActivity.java
+++ app/src/main/java/com/blockheads/mygbu/MapActivity.java
@@ -2,10 +2,14 @@ package com.blockheads.mygbu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class MapActivity extends AppCompatActivity {
+    ImageButton locset0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +19,18 @@ public class MapActivity extends AppCompatActivity {
             getSupportActionBar().hide();
         }
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+        locset0 = findViewById(R.id.locset0);
+
+        locset0.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gmmIntentUri = Uri.parse("google.navigation:q=Bodhisattva Dr. B.R. Ambedkar Central Library, Gautam Buddha University (Delhi NCR)");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+
+            }
+        });
     }
 }
