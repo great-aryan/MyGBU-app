2f14428f5366a2bcbeea6709fc1427a41085e2af
Aryan <aryans2808@gmail.com>
2023-04-07 03:27:56 +0530

    debug

--- app/src/main/java/com/blockheads/mygbu/MainActivity.java
+++ app/src/main/java/com/blockheads/mygbu/MainActivity.java
@@ -1,21 +1,16 @@
 package com.blockheads.mygbu;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-
 public class MainActivity extends AppCompatActivity {
     ImageView splash_logo;
     TextView splashgbu;
     TextView bhpro;
     float v=0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,25 +18,19 @@ public class MainActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
-
-
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         splash_logo = findViewById(R.id.splash_logo);
         splashgbu = findViewById(R.id.splashgbu);
         bhpro = findViewById(R.id.bhpro);
-
         splash_logo.setTranslationY(50);
         splashgbu.setTranslationY(200);
         bhpro.setTranslationY(300);
-
         splash_logo.setAlpha(v);
         splashgbu.setAlpha(v);
         bhpro.setAlpha(v);
-
         splash_logo.animate().translationY(0).alpha(1).setDuration(100).setStartDelay(10).start();
         splashgbu.animate().translationY(0).alpha(1).setDuration(500).setStartDelay(200).start();
         bhpro.animate().translationY(0).alpha(1).setDuration(500).setStartDelay(200).start();
-
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -53,7 +42,5 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         }, 1000);
-
-        }
     }
-
+}
